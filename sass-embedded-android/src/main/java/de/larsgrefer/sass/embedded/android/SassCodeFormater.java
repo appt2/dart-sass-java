7f658059817bb2fa47a09b67868f702a3c086aae
appt2 <143449333+appt2@users.noreply.github.com>
2024-03-10 01:31:49 +0330

    Create SassCodeFormater.java
    add code formater sass not full son fixed

---
+++ sass-embedded-android/src/main/java/de/larsgrefer/sass/embedded/android/SassCodeFormater.java
@@ -0,0 +1,14 @@
+package de.larsgrefer.sass.embedded.android;
+
+//for test not ful code soon fixed
+
+public class SassCodeFormater{
+ public String formatCode;
+  public SassCodeFormater(String formatCode){
+    this.formatCode = formatCode;
+  }
+
+  public String format(){
+   return formatCode;
+  }
+}
